dd9d81a90bb37fb4715ec3d8ecf27a0acf13ee6d
Israel Sanchez <Israel.pro1994@gmail.com>
2022-04-13 16:48:08 -0500

    checkout service imp created to do all work

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -1,15 +1,54 @@
 package com.luv2code.ecommerce.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.luv2code.ecommerce.dao.CustomerRepository;
 import com.luv2code.ecommerce.dto.*;
+import com.luv2code.ecommerce.entity.*;
+
+// now with all the models created as well as repository
+
+// CheckoutController => CheckoutService => Spring Data JPA Repository => DataBase
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
+	
+	private CustomerRepository customerRepository;
+	
+	public CheckoutServiceImpl( CustomerRepository customerRepository ) {
+		this.customerRepository = customerRepository;
+	};
 
 	@Override
+	@Transactional
 	public PurchaseResponse placeOrder(Purchase purchase) {
+		
+		// retrieve the order info from dto
+		Order order = purchase.getOrder();
+		
+		// generate tracking number
+		String orderTrackingNumber = generateOrderTrackingNumber();
+		order.setOrderTrackingNumber(orderTrackingNumber);
+		
+		// populate order with orderItems
+		
+		// populate order with billingAddress and shippingAddress
+		
+		// populate customer with order
+		
+		// save to the database
+		
+		// return a response 
 		return null;
 	};
+
+	private String generateOrderTrackingNumber() {
+		// TODO Auto-generated method stub
+		return null;
+	};
+	
+	
 	
 };
