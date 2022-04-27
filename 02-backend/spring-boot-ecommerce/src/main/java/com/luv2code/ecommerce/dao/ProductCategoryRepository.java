e45d867ed1869c29d62f305f14933a43d0c75f56
Israel Sanchez <Israel.pro1994@gmail.com>
2022-04-27 10:51:07 -0500

    hopefully I switched back correctly

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -4,7 +4,7 @@ import com.luv2code.ecommerce.entity.ProductCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource( collectionResourceRel = "productCategory", path = "product-category" )
 // "productCategory" <== name of JSON entry  "product-category" <== /product-category (url)
